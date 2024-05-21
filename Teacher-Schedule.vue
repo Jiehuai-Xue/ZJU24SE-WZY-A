d92f914e237197e43349e8b127fe655b89da1434
TripleCYZ <130230771+TripleCYZ@users.noreply.github.com>
2024-05-21 23:12:47 +0800

    Add files via upload

---
+++ Teacher-Schedule.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavBar title='制定培养方案' />
+  <TeacherSchedule />
+</template>
+
+<script setup lang="ts">
+
+import NavBar from "../components/NavBar.vue";
+import TeacherSchedule from "../components/TeacherSchedule.vue";
+</script>
+
+<style scoped>
+
+</style>
